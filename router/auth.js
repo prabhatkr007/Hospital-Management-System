d2816b232270395534c835ad95f8ccae711eec55
prabhatkr007 <pkr.876550@gmail.com>
2023-05-08 15:45:33 +0530

    ..

--- router/auth.js
+++ router/auth.js
@@ -121,7 +121,12 @@ router.get('/getdata', authenticate,(req,res)=>{
 
 
 router.get('/signout',authenticate,(req,res)=>{
-    res.clearCookie('jwtoken',{path:'/'});
+  res.clearCookie('jwtoken', {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none', 
+    path:'/', 
+    domain: 'mernproject-backend.onrender.com'});
     req.User.save;
     res.json({ message: 'User logout successfully', token });
     
